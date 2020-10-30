f6f4887829d43555517abdd7cb6b665ce82172c7
seiiedali <ali.miramirkhani@gmail.com>
2020-10-30 19:07:34 +0330

    updating Readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # vector space information retrieval model
- text query system using vector using vector space model, TF-IDF weighting and consine similarity method
+ This text query system uses vector vector space model for indexing extracted tokens from documents.
+ ## Indexing
+ Tokens will be parsed through each document text and after some cleaning data(punctuation, white spaces), will be index based on vector space model concepts. Tokenized 
+ word are stored in a dictionary with information about their repetition and associated document files. Finally tokens are inserted into another dictionary where their weight on TF-IDF scale are assessed. This data are ready to be any query process.
+ ## Query
+ After parsing the documents, program would ask you to enter a query up to 4 words. Entered query will be processed in the same way that document were parsed. Final result is ranked related documents which are assessed by Cosine Similarity formula based on TF-IDF values.
--- indexer.py
+++ indexer.py
@@ -1,9 +1,10 @@
 import os
 import math
 import operator
-import re
-import string
+#import re
+#import string
 from typing import Dict, List, Any
+
 
 def remove_punctuation(s):
     punctuation = "0123456789!()-[]{};:'\"\,<>./?@#$%^&*_~"
@@ -19,27 +20,27 @@ a   q
 for root, dirs, files in os.walk("./Docs/"):
     for filename in files:
         path = "./Docs/" + filename
-        #allFiles.append(filename)
+        # allFiles.append(filename)
         with open(path, "r") as content:
-            content = content.read().replace('\n',' ')
+            content = content.read().replace('\n', ' ')
             #cleanData = content.read().translate(str.maketrans('', '', string.punctuation)).lower()
             cleanData = remove_punctuation(content).lower()
             tokens = cleanData.split()
             allFiles[filename] = dict()
             for term in tokens:
                 if term in allFiles[filename]:
-                    allFiles[filename][term]+= 1
+                    allFiles[filename][term] += 1
                 else:
                     allFiles[filename][term] = 1
             uniqeTokens = list(dict.fromkeys(tokens))
-            #print(uniqeTokens)
+            # print(uniqeTokens)
             for word in uniqeTokens:
                 #info = (filename, index)
                 if word in indexTable:
                     indexTable[word] += 1
                 else:
                     indexTable[word] = 1
-                    #indexTable[word].append(info)
+                    # indexTable[word].append(info)
 '''
 for item in allFiles:
     print("in a file name " + str(item) + " --> " + str(allFiles[item]))
@@ -48,16 +49,18 @@ for item in indexTable:
 print("\n\n the all words counts are:" + str(indexTable.__len__()))
 '''
 tfIdf = dict()
-for filename in  :
-    #print(filename)
+for filename in :
+    # print(filename)
     tfIdf[filename] = dict()
     for word in allFiles[filename]:
-        tfIdf[filename][word] = allFiles[filename][word] * math.log10(allFiles.__len__()/indexTable[word])
+        tfIdf[filename][word] = allFiles[filename][word] * \
+            math.log10(allFiles.__len__()/indexTable[word])
 
 for item in tfIdf:
     print("in a file name " + str(item) + " --> " + str(tfIdf[item]))
 for item in indexTable:
-    print("\"" + str(item) + "\"" + " wtih the weight of: " + str(indexTable[item]))
+    print("\"" + str(item) + "\"" +
+          " wtih the weight of: " + str(indexTable[item]))
 
 print("dictionary is made, data is loaded ...")
 
@@ -65,13 +68,15 @@ print("dictionary is made, data is loaded ...")
 query = input("enter your query").lower()
 queryTokens = query.split()
 
-queryVector = {i:queryTokens.count(i) for i in queryTokens}
+queryVector = {i: queryTokens.count(i) for i in queryTokens}
 
 for item in queryVector:
     if item in indexTable:
-        queryVector[item] = queryVector[item] * math.log10(allFiles.__len__()+1/indexTable[item]+1)
+        queryVector[item] = queryVector[item] * \
+            math.log10(allFiles.__len__()+1/indexTable[item]+1)
     else:
-        queryVector[item] = queryVector[item] * math.log10(allFiles.__len__() + 1)
+        queryVector[item] = queryVector[item] * \
+            math.log10(allFiles.__len__() + 1)
 
 print(queryVector)
 
@@ -103,10 +108,10 @@ for filename in finalTable:
         queryValue += math.sqrt(finalQuery[word] ** 2)
     rank[filename] = innerProduct / (docValue * queryValue)
 
-ranklist = sorted(rank.items(), key=operator.itemgetter(1) , reverse=True)
+ranklist = sorted(rank.items(), key=operator.itemgetter(1), reverse=True)
 counter = 1
 for value in ranklist:
     print(str(counter) + ". " + str(value))
     counter += 1
 
-#print(ranklist)
+# print(ranklist)
